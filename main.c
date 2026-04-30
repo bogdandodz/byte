eeca11e4d6306369cd9e0bccf3ff88f4459420d1
bogdandodz <dodzbogdan@gmail.com>
2026-04-30 16:34:22 +0300

    Added some value checking

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 int main(int argc, char *argv[])
 {
     float mb = atof(argv[1]);
+    if (mb < 0) {
+        printf("Enter a valid value\n");
+        return 1;
+    }
+    else {
     printf("%.2f MB = %.0f Bytes\n", mb, mb * 1024 *1024);
+    
     return 0;
+    }
 }
